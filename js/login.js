59101f7b89c8d069fa614542d69557a63dff5523
Keith Mattison <klmattis@ncsu.edu>
2019-05-14 20:09:45 -0400

    dropping React/Semantic for lighter-weight approach

--- js/login.js
+++ js/login.js
@@ -3,52 +3,14 @@
  *  @author  Keith Mattison
  *  @class  Login component
  */
-class Login extends React.Component {
-    // Set initial state
-    state = {
-        loggedIn: false
-    };
-
-    // Attempt to login using user's key file, set state based on success or failure
-    login = (files) => {
-        var fr = new FileReader();
-        fr.onload = (ev) => {
-            try {
-                wallet = JSON.parse(ev.target.result);
-                this.setState({ loggedIn: true });
-            } catch (err) {
-                alert('Error logging in: ' + err);
-                this.setState({ loggedIn: false });
-            }
+login = (files) => {
+    var fr = new FileReader();
+    fr.onload = (ev) => {
+        try {
+            wallet = JSON.parse(ev.target.result);
+        } catch (err) {
+            alert('Error logging in: ' + err);
         }
-        fr.readAsText(files[0]);
     }
-
-    // Render Login and Prediction
-    render() {        
-        const loggedIn = this.state.loggedIn ? {} : {display: 'none'};
-        const notLoggedIn = this.state.loggedIn ? {display: 'none'} : {};
-        console.log(this.props);
-        return (
-            <div id="login-prediction-container">
-                <div id="login-container" className="large-padding-top">
-                    <div id="login-file-container" className="file-input" style={notLoggedIn}>
-                        <input id="login-file-input" type="file" onChange={(e) => {this.login(e.target.files)}} />
-                        <div id="login-file-description">
-                            DROP YOUR KEY BELOW TO LOGIN AND CREATE A PREDICTION
-                        </div>
-                    </div>
-                </div>
-                <div id="prediction-outer-container" style={loggedIn}>
-                    <Prediction />
-                </div>
-            </div>
-        );
-    }
+    fr.readAsText(files[0]);
 }
-
-// Render login to DOM element
-ReactDOM.render(
-    <Login />, 
-    document.getElementById("login-prediction")
-);
